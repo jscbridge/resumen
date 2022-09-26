f871dddf473f11f9f4b56b6d7266dcac3b8a0828
jscbridge <jorge.sanchez@thebridgeschool.es>
2022-09-26 09:53:29 +0200

    funciones

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -1,29 +1,33 @@
 function saludo() {
-  console.log("Hola Mundo 1");
+  console.log('Hola Mundo 1')
 }
 
-saludo();
+saludo()
 
 function saludo2() {
-  return console.log("Hola Mundo 2 ");
+  return console.log('Hola Mundo 2 ')
 }
 
-saludo4();
+saludo4()
 
 function saludo3() {
-  var frase = "Hola mundo 3";
-  return frase;
+  var frase = 'Hola mundo 3'
+  return frase
 }
 
-saludo2();
-saludo3();
+saludo2()
+console.log(saludo3())
 
 function saludo4() {
-  return "Hola mundo 4";
+  return 'Hola mundo 4'
 }
 
 function saludo5() {
-  return 2 + 3;
+  return 2 + 3
 }
 
-console.log(saludo5());
+saludo5()
+
+var string = prompt("Introduce un número") // 3 en string
+console.log(typeof(string))  // cadena
+console.log(typeof(parseInt(string)))  /numero
